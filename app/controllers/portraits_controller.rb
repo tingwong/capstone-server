f2419b8ac6a0f2467e0325467fc7c59abce4fece
Ting Wong <hellotingwong@gmail.com>
2017-07-12 13:51:55 -0700

    folder for images

--- app/controllers/portraits_controller.rb
+++ app/controllers/portraits_controller.rb
@@ -6,13 +6,16 @@ class PortraitsController < ApplicationController
 
   def process_image
     # Write binary image to file
-    File.open('test.png',"wb") do |file|
+    File.open('static/test.png',"wb") do |file|
       file.write(Base64.decode64(params[:canvasURL]))
     end
 
     render status: :ok, json: {}
     # Resize canvas
-
+    `python ../tools/dockrun.py python ../tools/process.py \
+      --input_dir capstone-server/static \
+      --operation resize \
+      --output_dir resized`
 
     # Process canvas to portrait
     # Combine canvas with portraits
