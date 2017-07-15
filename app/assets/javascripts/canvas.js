bc5dbf4a2e4e8b84d6cae3c196f03cca8b1e089a
Ting Wong <hellotingwong@gmail.com>
2017-07-15 15:26:01 -0700

    changed file path again

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -33,7 +33,7 @@ $(document).ready( function() {
         console.log(data.file_name);
 
         document.getElementById('imageDiv')
-        .innerHTML = ('<img src="capstone/pix2pix-tensorflow/test-output/' + data.file_name + '" alt="processed_portrait">');
+        .innerHTML = ('<img src="../../../capstone/pix2pix-tensorflow/test-output/' + data.file_name + '" alt="processed_portrait">');
       },
       error:function(data){
         debugger;
