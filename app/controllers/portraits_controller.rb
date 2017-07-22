96decc3c4425fa955b3f4cec2367977367624092
Ting Wong <hellotingwong@gmail.com>
2017-07-22 15:18:32 -0700

    changing order

--- app/controllers/portraits_controller.rb
+++ app/controllers/portraits_controller.rb
@@ -5,7 +5,7 @@ class PortraitsController < ApplicationController
   end
 
   def gallery
-    @files_sorted_by_time = Dir['/home/ubuntu/capstone/pix2pix-tensorflow/test-output/*'].sort_by{ |f| File.ctime(f) }.first(10).reverse
+    @files_sorted_by_time = Dir['/home/ubuntu/capstone/pix2pix-tensorflow/test-output/*'].sort_by{ |f| File.ctime(f) }.reverse.first(10)
     render status: :ok, json: {files_sorted_by_time: @files_sorted_by_time}
   end
 
