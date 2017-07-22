f1003dc469d60ae921cb1f628d6c309b0bede4c3
Ting Wong <hellotingwong@gmail.com>
2017-07-22 15:10:56 -0700

    naming change

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -5,7 +5,7 @@ function imageList(data) {
   for (var i = 0; i < data.length; i++) {
     // document.getElementById('imageGallery')
     // .innerHTML += ('<li>' + '<img src="' + BASE_URL + 'capstone/pix2pix-tensorflow/test-output/' + data.files_sorted_by_time[i] + '" alt="processed_portrait">' + '</li>');
-    console.log(('<li>' + '<img src="' + BASE_URL + 'capstone/pix2pix-tensorflow/test-output/' + data[i].slice(53, -1) + '" alt="processed_portrait">' + '</li>'));
+    console.log(('<li>' + '<img src="' + BASE_URL + 'capstone/pix2pix-tensorflow/test-output/' + data[i].slice(53) + '" alt="processed_portrait">' + '</li>'));
   }
 }
 
